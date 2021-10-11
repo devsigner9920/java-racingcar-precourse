f16613b0ae85cfc7c76d3e4c9ab4f23f9788e362
devsigner <devsigner9920@icloud.com>
2021-10-11 21:54:04 +0900

    feat (CarTest): 자동차 객체 구현을 위한 테스트 작성
    추가 내용:
    - 레이싱 게임에서 자동차가 할 수 있는 행위에 대해 테스트 케이스를 먼저 작성하였습니다.

---
+++ src/test/java/racinggame/model/CarTest.java
@@ -0,0 +1,44 @@
+package racinggame.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+  private Car car;
+
+  private static boolean isMoving() {
+    return true;
+  }
+
+  private static boolean isNotMoving() {
+    return false;
+  }
+
+  @BeforeEach
+  void setUp() {
+    car = new Car("Tommy");
+  }
+
+  @Test
+  @DisplayName("Car 객체 생성")
+  public void makeCar() {
+    assertThat(car.getName()).isEqualTo("Tommy");
+  }
+
+  @Test
+  @DisplayName("자동차가 전진한다.")
+  public void moveCar() {
+    car.move(CarTest::isMoving);
+    assertThat(car.getPosition()).isEqualTo(1);
+  }
+
+  @Test
+  @DisplayName("자동차가 멈춰있다.")
+  public void notMoveCar() {
+    car.move(CarTest::isNotMoving);
+    assertThat(car.getPosition()).isEqualTo(0);
+  }
+}
