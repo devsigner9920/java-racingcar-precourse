8381a09a9a732f9b040a1c077e0e3cf07e147ebb
devsigner <devsigner9920@icloud.com>
2021-10-13 01:38:57 +0900

    feat (OutputMessage): 출력 메시지를 가지는 constant 클래스 생성
    추가 내용:
    - 프로그램에서 출력되는 예외가 아닌 메시지들을 관리하기 위한 클래스를 생성하였습니다.

---
+++ src/main/java/racinggame/constant/OutputMessage.java
@@ -0,0 +1,11 @@
+package racinggame.constant;
+
+public class OutputMessage {
+  public static final String RACING_GAME_EACH_RESULT_MESSAGE = "%s : %s";
+  public static final String RACING_GAME_RESULT_START_MESSAGE = "실행 결과";
+  public static final String RACING_GAME_WINNER_MESSAGE = "최종 우승자는 %s 입니다.";
+  public static final String RACING_GAME_INPUT_CAR_NAMES_GUIDE_MESSAGE =
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+  public static final String RACING_GAME_INPUT_GAME_COUNT_GUIDE_MESSAGE =
+    "시도할 회수는 몇회인가요?";
+}
